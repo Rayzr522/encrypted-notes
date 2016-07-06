c393872441556bb4b86160e366310895d58db108
Yoni Lerner <yonilerner@gmail.com>
2016-07-05 23:18:35 -0700

    Fixing dependencies

--- app/app.js
+++ app/app.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactTestUtils from 'react-addons-test-utils'
 import {BasicRouter} from './components/index.js'
-import db from './utils/localStorage'
 import DBManager from './backend/db'
-import Encryption from './backend/encryption'
 
 DBManager.checkDatabaseInitialization()
 
 ReactDOM.render(<BasicRouter/>, document.getElementById('app'))
-
-window.test = ReactTestUtils
-window.ReactDOM = ReactDOM
-window.React = React
-window.DBManager = DBManager
-window.db = db
-window.enc = Encryption
